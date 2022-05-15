a6fd6eaed263f50e8abb3a6951f6df17ae9fe1a3
re4ever <re4ever@naver.com>
2022-05-15 23:47:57 +0900

    한섭적용

--- 3.GuideTranslate.bat
+++ 3.GuideTranslate.bat
@@ -1,3 +1,2 @@
 @echo off
-
-.\bin\AGI18nTranslator.exe Guide .\GuideCNFiles.txt .\GuideJAPFiles.txt .\GuideTWFiles.txt .\GuideSheet.csv .\ReplaceText.tsv .\IgnoreText.tsv
+.\bin\AGI18nTranslator.exe Guide .\GuideCNFiles.txt .\GuideJAPFiles.txt .\GuideTWFiles.txt .\GuideKORFiles.txt .\GuideSheet.csv .\ReplaceText.tsv .\IgnoreText.tsv .\OverwriteText.tsv
--- 4.HelpTranslate.bat
+++ 4.HelpTranslate.bat
@@ -1,3 +1,1 @@
-@echo off
-
 .\bin\AGI18nTranslator.exe .\HelpCNFiles.txt .\HelpJAPFiles.txt .\HelpTWFiles.txt .\Help_CN_Sheet.csv .\Help_JAP_Sheet.csv .\ReplaceText.tsv
