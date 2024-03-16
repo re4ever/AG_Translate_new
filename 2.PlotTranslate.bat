16391c5bf5054e7338eb3e120cefb33fe1af0322
re4ever <re4ever@naver.com>
2024-03-17 02:21:26 +0900

    Update

--- 2.PlotTranslate.bat
+++ 2.PlotTranslate.bat
@@ -1,5 +1,5 @@
 @echo off
 
-.\bin\AGPlotTranslator.exe .\PlotCNFiles.txt .\PlotJAPFiles.txt  .\ReplaceText.tsv .\Plot_CN_ContentSheet.csv .\Plot_CN_NameSheet.csv .\Plot_CN_OptionSheet.csv .\Plot_JAP_ContentSheet.csv .\Plot_JAP_NameSheet.csv .\Plot_JAP_OptionSheet.csv .\ForceCNPlotRange.tsv .\PlotTWFiles.txt .\PlotKORFiles.txt .\PlotKORSheet.tsv
+.\bin\ArteryGearTranslator.exe Plot .\PlotCNFiles.txt .\PlotJAPFiles.txt  .\ReplaceText.tsv .\Plot_CN_ContentSheet.csv .\Plot_CN_NameSheet.csv .\Plot_CN_OptionSheet.csv .\Plot_JAP_ContentSheet.csv .\Plot_JAP_NameSheet.csv .\Plot_JAP_OptionSheet.csv .\ForceCNPlotRange.tsv .\PlotTWFiles.txt .\PlotKORFiles.txt .\PlotKORSheet.tsv
 
 pause
